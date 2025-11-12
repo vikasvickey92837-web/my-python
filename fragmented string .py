e619dd54dc1764a2736beff7cca2d9efac99990e
vikasvickey92837-web <vikasvickey92837@gmail.com>
2025-11-12 12:18:01 +0530

    Create fragmented string .py

---
+++ fragmented string .py
@@ -0,0 +1,7 @@
+name="vikas c b"
+age=19
+college="siet"
+deparatment="AIML"
+print(f"my name is:{name} and my age is: {age} i am studying at: {college} in deparatment:{deparatment}")
+
+my name is:vikas c b and my age is: 19 i am studying at: siet in deparatment:AIML
