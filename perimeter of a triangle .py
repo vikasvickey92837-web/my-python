c7681fcf35701bc505728661ad23a7042130477a
vikasvickey92837-web <vikasvickey92837@gmail.com>
2025-11-12 18:53:55 +0530

    Create perimeter of a triangle .py

---
+++ perimeter of a triangle .py
@@ -0,0 +1,10 @@
+side1=float(input("enter the first side of the triangle: "))
+side2=float(input("enter the second side of the triangle: "))
+side3=float(input("enter the third side of the triangle: "))
+perimeter=side1+side2+side3
+print("the perimeter of the triangle is:",perimeter)
+
+enter the first side of the triangle: 3
+enter the second side of the triangle: 3
+enter the third side of the triangle: 2
+the perimeter of the triangle is: 8.0
