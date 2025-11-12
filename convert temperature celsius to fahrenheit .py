ba78771a5a51cc2178c8a1167a914609a743bd14
vikasvickey92837-web <vikasvickey92837@gmail.com>
2025-11-12 18:51:50 +0530

    Create convert temperature celsius to fahrenheit .py

---
+++ convert temperature celsius to fahrenheit .py
@@ -0,0 +1,6 @@
+celsius=float(input("enter temperature in celsius: "))
+fahrenheit=(celsius*9/5)+32
+print("temperature in fahrenheit:",fahrenheit)
+
+enter temperature in celsius: 35
+temperature in fahrenheit: 95.0
