17b69f2f87ed6ed1541d3f96cf3ebabee9a2fa8d
vikasvickey92837-web <vikasvickey92837@gmail.com>
2025-11-12 19:58:32 +0530

    Create entered by the user .py

---
+++ entered by the user .py
@@ -0,0 +1,9 @@
+num1=float(input("enter first number: "))
+num2=float(input("enter second number: "))
+sum=num1+num2
+print("the sum of",num1,"and",num2,"is:",sum)
+
+
+enter first number: 5
+enter second number: 6
+the sum of 5.0 and 6.0 is: 11.0
