3ce7dbdcb0c8366eb302fed94d5ec91cfa069d1f
vikasvickey92837-web <vikasvickey92837@gmail.com>
2025-11-12 12:57:48 +0530

    Create grade calucation.py

---
+++ grade calucation.py
@@ -0,0 +1,12 @@
+a=int(input("enter the marks"))
+if a>=90:
+  print("excellent")
+elif a>=80:
+  print("good")
+elif a>=60:
+  print("average")
+elif a<=50:
+  print("fail")
+
+enter the marks90
+excellent
