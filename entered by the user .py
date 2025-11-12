cc4438d66f06d6b7fbe26c1662f0591428a51587
vikasvickey92837-web <vikasvickey92837@gmail.com>
2025-11-12 18:17:44 +0530

    Create entered by the user .py

---
+++ entered by the user .py
@@ -0,0 +1,23 @@
+a=int(input("enter the first number: "))
+b=int(input("enter the second number: "))
+print("\nBefore swapping:")
+print("a=",a)
+print("b=",b)
+#use a temporary variable to swap
+temp=a
+a=b
+b=temp
+print("\nAftor swapping")
+print("a=", a)
+print("b", b)
+
+enter the first number: 2
+enter the second number: 3
+
+Before swapping:
+a= 2
+b= 3
+
+Aftor swapping
+a= 3
+b 2
