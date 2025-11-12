671a94c32f81f2c16aba3cdf851b435c73e02be5
vikasvickey92837-web <vikasvickey92837@gmail.com>
2025-11-12 18:45:29 +0530

    Update arithametic operation .py

--- arithametic operation .py
+++ arithametic operation .py
@@ -1,11 +1,27 @@
-length=float(input("enter the lenght of the rectangle: "))
-width=float(input("enter the width of the rectangle: "))
-area=length*width
-perimeter =  2 * (length + width)
-print("area of the rectangle", area)
-print("perimeter of the rectangle", perimeter)
+num1=float(input("enter the first number: "))
+num2=float(input("enter the second number: "))
+add=num1+num2
+sub=num1-num2
+mul=num1*num2
+div=num1/num2
+floordiv=num1//num2
+mod=num1%num2
+exp=num1**num2
+print("addition:", add)
+print("subraction:", sub)
+print("multipication:", mul)
+print("division:", div)
+print("floor division:", floordiv)
+print("modulas:", mod)
+print("exponetional:", exp)
 
-enter the lenght of the rectangle: 4
-enter the width of the rectangle: 6
-area of the rectangle 24.0
-perimeter of the rectangle 20.0
+enter the first number: 10
+enter the second number: 3
+addition: 13.0
+subraction: 7.0
+multipication: 30.0
+division: 3.3333333333333335
+floor division: 3.0
+modulas: 1.0
+exponetional: 1000.0
+
