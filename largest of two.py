25db4010b394cda14cb77de39178662fa69f1031
vikasvickey92837-web <vikasvickey92837@gmail.com>
2025-11-12 19:36:41 +0530

    Create largest of two.py

---
+++ largest of two.py
@@ -0,0 +1,11 @@
+def largest_of_two(a,b):
+  if a > b:
+    print(f"{a} is larger")
+  elif b > a:
+      print(f"{b} is larger")
+  else:
+      print("both are equal")
+largest_of_two(10,10)
+
+
+both ara equal
