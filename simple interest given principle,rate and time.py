54ce83cc659d4d4ac9798c02fa98e0c7970e5f50
vikasvickey92837-web <vikasvickey92837@gmail.com>
2025-11-12 19:56:02 +0530

    Create simple interest given principle,rate and time.py

---
+++ simple interest given principle,rate and time.py
@@ -0,0 +1,12 @@
+principal=float(input("enter the principal amount: "))
+rate=float(input("enter the rate of intersest(per annum): "))
+time=float(input("enter the time(in year): "))
+
+simple_interest=(principal*rate*time)/100
+
+print(f"|nThe simple interst is:{simple_interest}")
+
+enter the principal amount: 3000
+enter the rate of intersest(per annum): 12000
+enter the time(in year): 1
+|nThe simple interst is:360000.0
