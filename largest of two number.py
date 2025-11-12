b3db7459438b1efc8d647d5f04511bb2aa767857
vikasvickey92837-web <vikasvickey92837@gmail.com>
2025-11-12 13:00:56 +0530

    Create largest of two number.py

---
+++ largest of two number.py
@@ -0,0 +1,13 @@
+#largest of two number
+
+a=float(input("enter the first number: "))
+b=float(input("enter the second number: "))
+
+if a>b:
+  print("largest number is:",a)
+else:
+  print("largest number is:",b)
+
+enter the first number: 3
+enter the second number: 2
+largest number is: 3.0
