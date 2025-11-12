05bfe5da0163d587e0d1511e7651b1b73bc11576
vikasvickey92837-web <vikasvickey92837@gmail.com>
2025-11-12 19:51:54 +0530

    Create square and cube of givennumber.py

---
+++ square and cube of givennumber.py
@@ -0,0 +1,9 @@
+num=float(input("enter a number: "))
+square=num**2
+cube=num**3
+print("the square of",num,"is:", square)
+print("the cube of",num,"is:",cube)
+
+enter a number: 5
+the square of 5.0 is: 25.0
+the cube of 5.0 is: 125.0
