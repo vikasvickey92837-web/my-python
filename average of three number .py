1caaa730860520cf32db4f4c4079e8262e6c68c0
vikasvickey92837-web <vikasvickey92837@gmail.com>
2025-11-12 19:53:42 +0530

    Create average of three number .py

---
+++ average of three number .py
@@ -0,0 +1,10 @@
+num1=float(input("enter first number: "))
+num2=float(input("enter second number: "))
+num3=float(input("enter third: "))
+average=(num1+num2+num3)/3
+print('the average of the three numbers is:', average)
+
+enter first number: 3
+enter second number: 5
+enter third: 6
+the average of the three numbers is: 4.666666666666667
