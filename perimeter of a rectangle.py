4a4a94bcee8b8e979a19b2e7303a5be3f75b7278
vikasvickey92837-web <vikasvickey92837@gmail.com>
2025-11-12 18:22:15 +0530

    Create perimeter of a rectangle.py

---
+++ perimeter of a rectangle.py
@@ -0,0 +1,11 @@
+length=float(input("enter the lenght of the rectangle: "))
+width=float(input("enter the width of the rectangle: "))
+area=length*width
+perimeter =  2 * (length + width)
+print("area of the rectangle", area)
+print("perimeter of the rectangle", perimeter)
+
+enter the lenght of the rectangle: 4
+enter the width of the rectangle: 6
+area of the rectangle 24.0
+perimeter of the rectangle 20.0
