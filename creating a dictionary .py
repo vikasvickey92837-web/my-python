671d4822f5304616f7cc441f83e207b062059bf4
vikasvickey92837-web <vikasvickey92837@gmail.com>
2025-11-14 11:20:02 +0530

    Create creating a dictionary .py

---
+++ creating a dictionary .py
@@ -0,0 +1,11 @@
+# creating a dictionary 
+vikas={
+    "name"   : "rajesh",
+     "age"   :  19,
+     "marks" :  90,
+    "college": "shridevi",
+    "department": "aiml"   
+}
+print(vikas)
+
+{'name': 'rajesh', 'age': 19, 'marks': 90, 'college': 'shridevi', 'department': 'aiml'}
